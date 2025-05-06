ddeb410f6906d39442270ab78809b5750361e795
dajoh99 <dajoh99@morgan.edu>
2025-05-06 14:23:44 -0400

    more db stuff

--- backend-api/models.py
+++ backend-api/models.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
-
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
 class Transaction(BaseModel):
     id: str
     date: str
@@ -7,3 +9,13 @@ class Transaction(BaseModel):
     expenses: float
     balance: float
     goal: float
+# This is the SQLAlchemy Base for database models
+Base = declarative_base()
+class TransactionDB(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
+    amount = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
